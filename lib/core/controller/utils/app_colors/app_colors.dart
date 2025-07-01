2b469ba7ef873176695586e8ac1e0bf0e86317ae
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-01 10:06:15 -0700

    .

---
+++ lib/core/controller/utils/app_colors/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const greenColor = Color(0xff1D7885); // Greenish tone
+  static const darkTextColor= Color(0xFF4C4C6D); // Dark text
+  static const backgroundColor= Color(0xFFF7F9FC); // Light grey
+  static const pinkColor= Color(0xffBC439A); // Red for alerts
+  static const successColor = Color(0xffAFCB81);
+  static const purpleColor= Color(0xffB385FF); // Red for alerts
+  static const ligtgreen = Color(0xff3586A9);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color lightText = Color(0xFF9E9E9E);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
