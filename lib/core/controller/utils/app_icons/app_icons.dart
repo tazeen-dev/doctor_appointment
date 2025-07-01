c0f43a87a1be3e9e6211018e566d5f90330057ce
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-01 10:39:42 -0700

    .

---
+++ lib/core/controller/utils/app_icons/app_icons.dart
@@ -0,0 +1,9 @@
+class AppIcons {
+  static const String basePath = 'assets/icons/';
+
+  static const String home = '${basePath}home_icon.png';
+  static const String doctor = '${basePath}doctor_icon.png';
+  static const String calendar = '${basePath}calendar_icon.png';
+  static const String chat = '${basePath}chat_icon.png';
+  static const String profile = '${basePath}profile_icon.png';
+}
---
+++ lib/core/controller/utils/app_images/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+  static const String welcomeBg = '${basePath}welcome_bg.jpg';
+  static const String doctor1 = '${basePath}doctor1.png';
+  static const String clinicBanner = '${basePath}clinic_banner.jpg';
+static const String backgroundimage='${basePath}background_image.jpg';
+}
