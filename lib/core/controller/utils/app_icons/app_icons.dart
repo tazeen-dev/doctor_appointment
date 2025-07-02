6e41f771e15935c5fc29035d74dd140148d1f560
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-01 19:50:43 -0700

    added images

--- lib/core/controller/utils/app_icons/app_icons.dart
+++ lib/core/controller/utils/app_icons/app_icons.dart
@@ -2,6 +2,9 @@ class AppIcons {
   static const String basePath = 'assets/icons/';
 
   static const String splash = '${basePath}doctorlogo.jpg';
+  static const String child = '${basePath}child.png';
+  static const String oldwomen = '${basePath}oldladdi.png';
+  static const String girl = '${basePath}girl.png';
   static const String doctor = '${basePath}doctor_icon.png';
   static const String calendar = '${basePath}calendar_icon.png';
   static const String chat = '${basePath}chat_icon.png';
--- lib/core/controller/utils/app_images/app_images.dart
+++ lib/core/controller/utils/app_images/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages {
   static const String basePath = 'assets/images/';
   static const String welcomeBg = '${basePath}welcome_bg.jpg';
-  static const String doctor1 = '${basePath}doctor1.png';
+  static const String doctor1 = '${basePath}femaledoctor.png';
   static const String clinicBanner = '${basePath}clinic_banner.jpg';
 static const String backgroundimage='${basePath}image 44.png';
 }
