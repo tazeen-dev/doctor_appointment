813531f715df8b172d4cbd6365e07720a5d3664f
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-01 21:43:43 -0700

    ..

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+routes: {
+'/welcome': (context) => const WelcomeScreen(),
+'/login': (context) => const LoginScreen(),
+'/signup': (context) => const SignUpScreen(),
+}
