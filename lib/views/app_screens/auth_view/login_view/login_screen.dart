fd8c1437d943fd79387c9970857f2abfd44bf298
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-03 21:36:46 -0700

    .

--- lib/views/app_screens/auth_view/login_view/login_screen.dart
+++ lib/views/app_screens/auth_view/login_view/login_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+import '../../../../core/controller/components/textfield/textfield.dart';
+import '../signup_view/signup_view.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -7,8 +11,80 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  bool _obscure = true;
+
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+            horizontal: screenWidth < 600 ? 24 : screenWidth * 0.2,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                CustomTextFormField(
+                  label: "Email",
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) => value != null && value.contains("@")
+                      ? null
+                      : "Enter a valid email",
+                  onSaved: (value) => email = value!,
+                ),
+                CustomTextFormField(
+                  label: "Password",
+                  obscureText: _obscure,
+                  validator: (value) => value != null && value.length >= 6
+                      ? null
+                      : "Password too short",
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _obscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () => setState(() => _obscure = !_obscure),
+                  ),
+                  onSaved: (value) => password = value!,
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        // TODO: Login logic
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Logged in")),
+                        );
+                      }
+                    },
+                    child: const Text("Login"),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                   Navigator.pushNamed(context, '/signup');
+                  },
+                  child: const Text("Don't have an account? Sign up"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
--- lib/views/app_screens/auth_view/signup_view/signup_view.dart
+++ lib/views/app_screens/auth_view/signup_view/signup_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import '../../../../core/controller/components/textfield/textfield.dart';
+
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
 
@@ -7,8 +10,84 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  bool _obscure = true;
+
+  String name = '';
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+            horizontal: screenWidth < 600 ? 24 : screenWidth * 0.2,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Text(
+                  "Sign Up",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                CustomTextFormField(
+                  label: "Full Name",
+                  validator: (value) =>
+                  value != null && value.isNotEmpty ? null : "Enter name",
+                  onSaved: (value) => name = value!,
+                ),
+                CustomTextFormField(
+                  label: "Email",
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) =>
+                  value != null && value.contains("@") ? null : "Invalid email",
+                  onSaved: (value) => email = value!,
+                ),
+                CustomTextFormField(
+                  label: "Password",
+                  obscureText: _obscure,
+                  validator: (value) => value != null && value.length >= 6
+                      ? null
+                      : "Minimum 6 characters",
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _obscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () => setState(() => _obscure = !_obscure),
+                  ),
+                  onSaved: (value) => password = value!,
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        // TODO: Signup logic
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Signed up")),
+                        );
+                      }
+                    },
+                    child: const Text("Sign Up"),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("Already have an account? Login"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
